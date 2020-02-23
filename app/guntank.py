5d0e539db8012dc9f5fb03ab1b3fef0a746ee47e
nevernothingjp <phantom@nevernothing.jp>
2020-02-23 13:23:47 +0900

    スペルミス

--- app/guntank.py
+++ app/guntank.py
@@ -2,36 +2,36 @@ import webiopi
 import subprocess as proc
 webiopi.setDebug()
 GPIO = webiopi.GPIO
-MORTOR_RIGHT_FORWARD = 2
-MORTOR_RIGHT_BACK = 3
-MORTOR_LEFT_FORWARD = 4
-MORTOR_LEFT_BACK = 14
-MORTOR_STOP = 15
+MOTOR_RIGHT_FORWARD = 2
+MOTOR_RIGHT_BACK = 3
+MOTOR_LEFT_FORWARD = 4
+MOTOR_LEFT_BACK = 14
+MOTOR_STOP = 15
 SHUTDOWN = 18
 
 def setup():
-  GPIO.setFunction(MORTOR_RIGHT_FORWARD, GPIO.OUT)
-  GPIO.setFunction(MORTOR_RIGHT_BACK, GPIO.OUT)
-  GPIO.setFunction(MORTOR_LEFT_FORWARD, GPIO.OUT)
-  GPIO.setFunction(MORTOR_LEFT_BACK, GPIO.OUT)
-  GPIO.setFunction(MORTOR_STOP, GPIO.OUT)
+  GPIO.setFunction(MOTOR_RIGHT_FORWARD, GPIO.OUT)
+  GPIO.setFunction(MOTOR_RIGHT_BACK, GPIO.OUT)
+  GPIO.setFunction(MOTOR_LEFT_FORWARD, GPIO.OUT)
+  GPIO.setFunction(MOTOR_LEFT_BACK, GPIO.OUT)
+  GPIO.setFunction(MOTOR_STOP, GPIO.OUT)
   GPIO.setFunction(SHUTDOWN, GPIO.OUT)
 
 def allLow():
-  GPIO.digitalWrite(MORTOR_LEFT_FORWARD, GPIO.LOW)
-  GPIO.digitalWrite(MORTOR_LEFT_BACK, GPIO.LOW)
-  GPIO.digitalWrite(MORTOR_RIGHT_FORWARD, GPIO.LOW)
-  GPIO.digitalWrite(MORTOR_RIGHT_BACK, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_LEFT_FORWARD, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_LEFT_BACK, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_RIGHT_FORWARD, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_RIGHT_BACK, GPIO.LOW)
 
 def destroy():
   allLow()
-  GPIO.digitalWrite(MORTOR_STOP, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_STOP, GPIO.LOW)
   GPIO.digitalWrite(SHUTDOWN, GPIO.LOW)
 
 @webiopi.macro
 def home():
   allLow()
-  GPIO.digitalWrite(MORTOR_STOP, GPIO.LOW)
+  GPIO.digitalWrite(MOTOR_STOP, GPIO.LOW)
   GPIO.digitalWrite(SHUTDOWN, GPIO.LOW)
 
 @webiopi.macro
@@ -41,44 +41,44 @@ def stop():
 @webiopi.macro
 def leftForwad():
   allLow()
-  GPIO.digitalWrite(MORTOR_RIGHT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_FORWARD, GPIO.HIGH)
 
 @webiopi.macro
 def forwad():
   allLow()
-  GPIO.digitalWrite(MORTOR_LEFT_FORWARD, GPIO.HIGH)
-  GPIO.digitalWrite(MORTOR_RIGHT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_FORWARD, GPIO.HIGH)
 
 @webiopi.macro
 def rightForwad():
   allLow()
-  GPIO.digitalWrite(MORTOR_LEFT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_FORWARD, GPIO.HIGH)
 
 @webiopi.macro
 def leftTurn():
   allLow()
-  GPIO.digitalWrite(MORTOR_LEFT_BACK, GPIO.HIGH)
-  GPIO.digitalWrite(MORTOR_RIGHT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_FORWARD, GPIO.HIGH)
 
 @webiopi.macro
 def rightTurn():
   allLow()
-  GPIO.digitalWrite(MORTOR_RIGHT_BACK, GPIO.HIGH)
-  GPIO.digitalWrite(MORTOR_LEFT_FORWARD, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_FORWARD, GPIO.HIGH)
 
 @webiopi.macro
 def leftBack():
   allLow()
-  GPIO.digitalWrite(MORTOR_RIGHT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_BACK, GPIO.HIGH)
 
 @webiopi.macro
 def back():
   allLow()
-  GPIO.digitalWrite(MORTOR_RIGHT_BACK, GPIO.HIGH)
-  GPIO.digitalWrite(MORTOR_LEFT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_RIGHT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_BACK, GPIO.HIGH)
 
 @webiopi.macro
 def rightBack():
   allLow()
-  GPIO.digitalWrite(MORTOR_LEFT_BACK, GPIO.HIGH)
+  GPIO.digitalWrite(MOTOR_LEFT_BACK, GPIO.HIGH)
 
